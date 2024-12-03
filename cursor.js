4348f5b33a47ba3b39c14dbef684bf2bebba8a32
Oskar Kent <oskar@Oskars-MacBook-Pro.local>
2024-12-03 14:25:14 -0500

    Huge Update!

---
+++ cursor.js
@@ -0,0 +1,7 @@
+import { textFlag } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
+if (window.innerWidth >= 1024) {			
+	new textFlag({
+		text: "Mr Kitty Woos",
+		color: "#ffffff"
+	});
+}
