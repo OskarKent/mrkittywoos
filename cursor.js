2af5d2f4064e18206df9b23558f08c01ee6843e8
Oskar <oskarkent8@gmail.com>
2025-01-02 15:46:04 -0500

    text update

--- cursor.js
+++ cursor.js
@@ -1,8 +1,1 @@
-import { textFlag } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
-if (window.innerWidth >= 1024) {			
-	new textFlag({
-		text: "Mr Kitty Woos",
-		color: "#00ffff",
-		font: "audiowide"
-	});
-}
+import{textFlag}from "https://unpkg.com/cursor-effects@latest/dist/esm.js";if(window.innerWidth>=1024){new textFlag({text:"Mr Kitty Woos",color:"#00ffff",font:"audiowide"})}
