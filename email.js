e62a0bfbcefab6dcec6e053db9554991d611aef1
Oskar <oskarkent8@gmail.com>
2025-01-07 11:32:09 -0500

    Contact page

---
+++ email.js
@@ -0,0 +1,27 @@
+const message = document.getElementById('message');
+const name = document.getElementById('name');
+const submit = document.getElementById('submit');
+
+emailjs.init({
+  publicKey: 'qy4Hy6xZFsxY7Xirg'
+});
+
+const send = (event) => {
+  if (message.value && name.value) {    
+    event.preventDefault();
+    emailjs.send("mrkittywoos","template_vrrfyph",{
+      message: message.value,
+      name: name.value
+    }).then(() => {
+      alert('Email Sent! Click OK To Go Back To The Home Page.')
+      window.location.replace('index.html');
+    }).catch((error) => {
+      alert('Error! Email Not Sent.')
+      console.log("There Was An Error! ", error.text)
+    });
+  } else {
+    alert('Please enter your message!')
+  }
+}
+
+submit.addEventListener('click', send)
