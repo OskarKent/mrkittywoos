ba4a0644a88b3f87d0730797473732027c4b0939
Oskar <robkent1@me.com>
2025-02-03 13:05:15 -0500

    huge style update

--- cursor.js
+++
@@ -1,1 +0,0 @@
-import{textFlag}from "https://unpkg.com/cursor-effects@latest/dist/esm.js";if(window.innerWidth>=1024){new textFlag({text:"Mr Kitty Woos",color:"#00ffff",font:"audiowide"})}
--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 const message = document.getElementById('message');
 const name = document.getElementById('name');
 const submit = document.getElementById('submit');
+let sent = false
 
 emailjs.init({
   publicKey: 'qy4Hy6xZFsxY7Xirg'
 });
 
 const send = (event) => {
-  if (message.value && name.value) {    
+  if (message.value && name.value && sent === false) {    
     event.preventDefault();
     emailjs.send("mrkittywoos","template_vrrfyph",{
       message: message.value,
       name: name.value
     }).then(() => {
-      alert('Email Sent! Click OK To Go Back To The Home Page.')
-      window.location.replace('index.html');
+      alert('Email Sent!')
+      name.value = "";
+      submit.textContent = "Email Sent!"
+      message.value = "";
+      sent = true;
     }).catch((error) => {
       alert('Error! Email Not Sent.')
       console.log("There Was An Error! ", error.text)
     });
   } else {
-    alert('Please enter your name and your message!')
+    if (sent === false) {
+      alert('Please enter your name and your message!')
+    } else {
+    	alert("email already sent!")
+    }
   }
 }
 
